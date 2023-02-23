28e88cb5ce1579c6f696cc688752639c69cf3026
Mokhru <mokhru.niezmamadova@tekschool.us>
2023-02-23 17:38:19 -0500

    new practice coming

---
+++ src/test/GitPracticeTwo.java
@@ -0,0 +1,12 @@
+package test;
+
+public class GitPracticeTwo {
+
+	public static void main(String[] args) {
+		
+		String practice = "This is for push practice";
+		System.out.println(practice);
+
+	}
+
+}
